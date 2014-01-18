ff5afcd4bfcb4ed4be8040222abdd2bb1733b954
Henk J. Reder <hnk.reder@gmail.com>
2014-01-18 05:22:28 -0500

    First Ruby Site

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ProjectClarity::Application.config.secret_key_base = 'b8ba2cf08578ee5df27039c4524c2c369d43148cf9cda1c0c0bb0ead0632e164a1f8b6cd8d55b8feb4c3e0c0fac6d2be8a9cd64350699361c7dfb523c57f8c93'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ProjectClarity::Application.config.session_store :cookie_store, key: '_project_clarity_session'
