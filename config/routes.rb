2593b4df2833ab60580f68a46b017a71928eb2b1
Milt Reder <milt@anestuary.com>
2014-01-18 20:37:42 -0500

    specify root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 ProjectClarity::Application.routes.draw do
   resources :people
 
-  get "greetings/hello"
+  root "greetings#hello"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
