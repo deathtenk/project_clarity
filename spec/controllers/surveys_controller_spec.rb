761676a9547cf9dc5e80c47a19e0b6e937138abf
Milt Reder <milt@anestuary.com>
2014-01-20 20:43:50 -0500

    new action spec

--- spec/controllers/surveys_controller_spec.rb
+++ spec/controllers/surveys_controller_spec.rb
@@ -2,4 +2,13 @@ require 'spec_helper'
 
 describe SurveysController do
 
+  let(:valid_attributes) { FactoryGirl.attributes_for(:survey)}
+
+  describe "GET new" do
+    it "assigns a new survey as @survey" do
+      get :new
+      assigns(:survey).should be_a_new(survey)
+    end
+  end
+
 end
