eceb16672bcf87e918dcae39bb68c7318754072a
Milt Reder <milt@anestuary.com>
2014-01-20 21:06:50 -0500

    more failing tests for you, bro!

---
+++ spec/views/new.html.erb_spec.rb
@@ -0,0 +1,25 @@
+require "spec_helper"
+
+describe "surveys/new.html.erb" do
+  it "displays a form for a new survey" do
+    
+    render
+
+    rendered.should have_selector("form.new-survey")
+  end
+  
+  it "has a field for the survey name" do
+    
+    render
+    
+    rendered.should have_selector("form input.name")
+    
+  end
+  
+  it "has a save button" do
+    render
+    
+    rendered.should have_content("Save")
+  end
+
+end
