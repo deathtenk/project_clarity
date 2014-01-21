4e30b4479320f56aa3db0d965c9e6024385ae0fe
Milt Reder <milt@anestuary.com>
2014-01-20 21:48:34 -0500

    fixed my stupidness in the view test, form test pass with tweaks

--- spec/views/new.html.erb_spec.rb
+++ spec/views/new.html.erb_spec.rb
@@ -2,25 +2,29 @@ require "spec_helper"
 
 describe "surveys/new.html.erb" do
   it "displays a form for a new survey" do
+    assign(:survey, Survey.new)
     
     render
 
-    rendered.should have_content("New Survey")
-    rendered.should have_selector("form.new-survey")
+    rendered.should have_selector("form.new_survey")
   end
   
   it "has a field for the survey name" do
+    assign(:survey, Survey.new)
+    
     
     render
     
-    rendered.should have_selector("form input.name")
+    rendered.should have_selector("form input#survey_name")
     
   end
   
   it "has a save button" do
+    assign(:survey, Survey.new)
+    
     render
     
-    rendered.should have_content("Save")
+    rendered.should have_button("Save")
   end
 
 end
